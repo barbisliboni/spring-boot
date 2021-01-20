02a33a21a9e18ba6c869165e5fcc2e86bd0dddea
barbisliboni <niinaliboni@gmail.com>
2021-01-20 13:48:31 -0300

    Criação dos métodos de requisição POST, PUT e DELETE

--- blogPessoal/blogPessoal/src/main/java/org/Generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/blogPessoal/src/main/java/org/Generation/blogPessoal/controller/PostagemController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.Generation.blogPessoal.model.Postagem;
 import org.Generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,4 +77,61 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+
+	/************************************************/
+
+	@PostMapping
+	/*
+	 * o PostMapping é usado para lidar com o tipo POST do método da requisição
+	 */
+
+	// post é o nome do método
+	// Postagem em maiusculo é o parametro do método
+	// postagem em minusculo é o nome que damos ao parametro
+	/*
+	 * quando utilizamos um objeto grande e seguro, no caso, que o usuario nao
+	 * poderá ter acesso/ver, passaremos pelo corpo da requisição feita, logo,
+	 * usaremos o @RequestBody
+	 */
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+		/*
+		 * salvando na base de dados e retornando no body, obtendo assim, o end point de
+		 * postagem
+		 */
+		/*
+		 * nao usaremos o .ok, que atende ao protocolo http 200, e sim, o 201, para
+		 * isso, usaremos o CREATED
+		 * 
+		 * colocar o .body e chamar o repositorio, apos, chamar o método save, e colocar
+		 * como parametro a entidade postagem (pois postagem é o que foi recebido pelo
+		 * client)
+		 */
+
+	}
+
+	/************************************************/
+
+	@PutMapping
+	/*
+	 * o PutMapping é usado para lidar com o tipo PUT do método da requisição, no
+	 * caso do CRUD, o POST seria o UPDATE
+	 */
+
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+
+	/************************************************/
+
+	@DeleteMapping("/{id}")
+	/*
+	 * o DeleteMapping é usado para lidar com o tipo DELETE do método da requisição
+	 * o parametro é o recurso que será deletado, o qual sera escolhido pelo id
+	 */
+	public void delete(@PathVariable long id) { // como é void, nao utlizo a palavra reservada return
+		repository.deleteById(id);
+		// resposta padrao (ok) caso de certo, e uma mensagem de erro caso não funcione
+	}
+
 }
